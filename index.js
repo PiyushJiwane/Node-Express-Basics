2f48acbef4bf93c90ce13ca1ccabb74063e84ffb
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-14 20:12:23 +0530

    updated : use all() function so irrespective of the path string and irrespective of the HTTP method it will return the output

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get("/",(req,res)=>{
     console.log("user hit the first request");
     res.end("<h1>Hello Piyush Jiwane</h1>")
 })
+
+app.all("*",(req,res)=>{
+    res.status(404).send("<h1>Page Not Found !!</h1>")
+})
+
 app.listen(3000,()=>{
     console.log("server is listening to port : 3000");
 })
