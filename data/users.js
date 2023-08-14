4ff5f145a212e6c6481a8fe299e133d6c6be8675
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-14 23:13:36 +0530

    this file is working as a db file

---
+++ data/users.js
@@ -0,0 +1,13 @@
+const users=[
+    {
+        id:101,
+        name:"piyush Jiwane",
+        desc:"computer engineer"
+    },{
+        id:102,
+        name:"xyz",
+        desc:"unknown"
+    }
+]
+
+module.exports=users
