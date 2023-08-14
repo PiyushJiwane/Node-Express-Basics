b25d26627b16d1ec042ed2fc9c9c6943fa4f48ea
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-14 20:23:11 +0530

    error : this file contains a readFileSync() function which suppose to read html data and show it on screen, but instead, it is downloading that file : so not workig as expected

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express=require("express")
+const {readFileSync}=require("fs")
+
+const homePage=readFileSync("./html/home.html")
 
 const app=express()
 
 app.get("/",(req,res)=>{
     console.log("user hit the first request");
-    res.end("<h1>Hello Piyush Jiwane</h1>")
+    // res.end("<h1>Hello Piyush Jiwane</h1>")
+    res.status(200).send(homePage)
 })
 
 app.all("*",(req,res)=>{
