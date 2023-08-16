9a37bed16b9026d901d83428edf04ac480e208d3
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-16 23:55:22 +0530

    to demonstrate how to seprate it out the middleware code and router codes.

---
+++ main/middleware2.js
@@ -0,0 +1,16 @@
+const express=require("express")
+// Using this require we can seprate it out the middleware code and route code
+const logger=require("../middleware/logger")
+
+const app=express()
+
+// This will import middleware function for all other route in middleware.js file
+app.use(logger) // order is important, try to use app.use() before all routers path. 
+
+app.get("/",(req,res)=>{
+    console.log("Home Page");
+})
+
+app.listen(3000,()=>{
+    console.log("server is listening to port : 3000");
+})
