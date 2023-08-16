2d5327c0c918f50a09db1aee5e3fedcc6da4f9a2
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-17 00:10:59 +0530

    updated : This code is updated to process a request through multiple middlewares.

--- main/middleware2.js
+++ main/middleware2.js
@@ -1,13 +1,15 @@
 const express=require("express")
 // Using this require we can seprate it out the middleware code and route code
 const logger=require("../middleware/logger")
+const authorize=require("../middleware/authorize")
 
 const app=express()
 
 // This will import middleware function for all other route in middleware.js file
-app.use("/api",logger) // order is important, try to use app.use() before all routers path. 
+app.use([logger,authorize]) // order is important, try to use app.use() before all routers path. 
 
 app.get("/",(req,res)=>{
+    console.log(req.user);
     console.log("Home Page");
 })
 
