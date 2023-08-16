41bd9d92a26d49fc1e552b60989b500b4d6c23c9
PiyushJiwane <jiwanepiyush@gmail.com>
2023-08-17 00:27:13 +0530

    updatef : lean how to use moragn middleware to print the logs on terminal.

--- main/middleware2.js
+++ main/middleware2.js
@@ -1,4 +1,5 @@
 const express=require("express")
+const morgan=require("morgan")
 // Using this require we can seprate it out the middleware code and route code
 const logger=require("../middleware/logger")
 const authorize=require("../middleware/authorize")
@@ -6,10 +7,15 @@ const authorize=require("../middleware/authorize")
 const app=express()
 
 // This will import middleware function for all other route in middleware.js file
-app.use([logger,authorize]) // order is important, try to use app.use() before all routers path. 
+// app.use([logger,authorize]) // order is important, try to use app.use() before all routers path. 
+
+app.use(morgan('tiny'));
+// GET /morgan-logger/data 404 157 - 1.564 ms
+// By using moragn middleware, we can get an output in above format like = :method :url :status :res[content-length] - :response-time ms
+
 
 app.get("/",(req,res)=>{
-    console.log(req.user);
+    // console.log(req.user);
     console.log("Home Page");
 })
 
